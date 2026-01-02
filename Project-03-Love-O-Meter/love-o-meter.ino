e1f09b334d353f7341e09e40aa5d1a40333501a4
Chrisjan Alejandro <123671935+Alertify123@users.noreply.github.com>
2026-01-02 15:57:56 -0500

    love o meter code

---
+++ Project-03-Love-O-Meter/love-o-meter.ino
@@ -0,0 +1,50 @@
+const int sensorPin = A0;
+const float baselineTemp = 27.0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); // open a serial port, communication speed of 9600 bits/sec
+
+  for(int pinNumber = 2; pinNumber < 5; pinNumber++){
+    pinMode(pinNumber, OUTPUT);
+    digitalWrite(pinNumber, LOW);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int sensorVal = analogRead(sensorPin); // value (0 to 1023) representing the voltage of the pin
+
+Serial.print("Sensor Value: ");
+Serial.print(sensorVal);
+
+float voltage = (sensorVal/1024.0) * 5.0; // conver the ADC reading to voltage
+
+Serial.print(" , Volts: ");
+Serial.print(voltage);
+
+Serial.print(" , Degrees C: ");
+// convert the voltage to temperature in degrees
+float temperature = (voltage - 0.5) * 100;
+Serial.println(temperature);
+
+if(temperature < baselineTemp){
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+}else if (temperature >= baselineTemp + 2 && temperature < baselineTemp + 4){
+  digitalWrite(2, HIGH);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+}else if (temperature >= baselineTemp + 4 && temperature < baselineTemp + 6){
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+}else if (temperature >= baselineTemp + 6){
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+}
+delay(1);
+}
